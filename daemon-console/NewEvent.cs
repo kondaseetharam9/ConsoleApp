7889a7c074b2ef20a4fe0e300f1954e920424805
kondaseetharam9 <75688362+kondaseetharam9@users.noreply.github.com>
2021-01-11 17:12:41 +0530

    Add files via upload

---
+++ daemon-console/NewEvent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace daemon_console
+{
+    public class NewEvent
+    {
+        public string subject;
+        public Start start;
+        public End end;
+        public bool isOnlineMeeting;
+        public string onlineMeetingProvider;
+        public IList<Attendee> attendees;
+        public class Start
+        {
+            public string dateTime;
+            public string timeZone;
+        }
+
+        public class End
+        {
+            public string dateTime;
+            public string timeZone;
+        }
+
+        public class EmailAddress
+        {
+            public string address;
+            public string name;
+
+            
+        }
+
+        public class Attendee
+        {
+            public EmailAddress emailAddress;
+            public string type;
+
+        }
+
+
+    }
+
+
+}
